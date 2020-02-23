56288c2a172d8098a467335e9f46fea0b3966c93
Orlando Quero <orlaqp@gmail.com>
2020-02-23 10:16:22 -0500

    reactive-login-done

--- libs/login/reactive/src/lib/components/login-reactive/login-reactive.component.ts
+++ libs/login/reactive/src/lib/components/login-reactive/login-reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'afs-login-reactive',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginReactiveComponent implements OnInit {
 
+  loginForm = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)])
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+    this.loginForm.valueChanges.subscribe(value => console.log(JSON.stringify(value)));
+  }
+
+  onSubmit() {
+    console.log('submitting form');
+
   }
 
 }
