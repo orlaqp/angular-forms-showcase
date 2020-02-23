d3dded0232cf186e3030d378f615ab8693277cd3
Orlando Quero <orlaqp@gmail.com>
2020-02-23 10:19:56 -0500

    reactive login completed

--- libs/login/reactive/src/lib/login-reactive.module.ts
+++ libs/login/reactive/src/lib/login-reactive.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '@angular-forms-showcase/material';
 import { LoginReactiveComponent } from './components/login-reactive/login-reactive.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, ReactiveFormsModule],
   exports:[LoginReactiveComponent],
   declarations: [LoginReactiveComponent]
 })
